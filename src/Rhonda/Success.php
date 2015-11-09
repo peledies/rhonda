bec936e95ffcabef69b6acbd83f6ed3940e233fe
Deac <deac.karns@gmail.com>
2015-11-09 14:26:50 -0500

    fixed syntax error in success method

--- src/Rhonda/Success.php
+++ src/Rhonda/Success.php
@@ -11,34 +11,34 @@ namespace Rhonda;
 */
 class Success
 {
-    /**
-    * Create a uniform success message string
-    *
-    * @example
-    * <code>
-    *   \Rhonda\Success::create();
-    * </code>
-    *
-    * Default success message
-    * @example
-    * <code>
-    *   $message = new \Rhonda\Success();
-    *   $message->create();
-    * </code>
-    *
-    * Example with custom message
-    * @example
-    * <code>
-    *   $message = new \Rhonda\Success();
-    *   $message->create("foo","bar");
-    * </code>
-    *
-    * @return String - Success message string
-    *
-    * @since   2015-11-09
-    * @author  Deac Karns <deac@sdicg.com> 
-    **/
-    public static function create($key=NULL,$value=TRUE)
+  /**
+  * Create a uniform success message string
+  *
+  * @example
+  * <code>
+  *   \Rhonda\Success::create();
+  * </code>
+  *
+  * Default success message
+  * @example
+  * <code>
+  *   $message = new \Rhonda\Success();
+  *   $message->create();
+  * </code>
+  *
+  * Example with custom message
+  * @example
+  * <code>
+  *   $message = new \Rhonda\Success();
+  *   $message->create("foo","bar");
+  * </code>
+  *
+  * @return String - Success message string
+  *
+  * @since   2015-11-09
+  * @author  Deac Karns <deac@sdicg.com> 
+  **/
+  public static function create($key=NULL,$value=TRUE){
     if(empty($key)){
       $key="success";
     }
