f865da72aa749039f5600ec183f7e9782b80776f
Deac <deac.karns@gmail.com>
2015-11-22 20:53:11 -0500

    updated tests

--- tests/test.php
+++ tests/test.php
@@ -95,3 +95,28 @@ echo "</br>";
 
 // Get all request headers
 print_r( \Rhonda\Headers:: getallheaders() );
+
+// Mysql escape 
+$object = json_decode('{
+  "connections": {
+    "local": {
+      "host": "192.168.99.100"
+    , "database": "core_elguapo"
+    , "port": "3306"
+    , "user": "root"
+    , "password": "SECRET"
+    }
+  }
+}');
+
+\Rhonda\Config::load_object('DB', $object);
+echo \Rhonda\Mysql:: real_escape("that's all folks");
+
+$object = new \stdClass();
+$object->obj = "it's fo sho";
+$object->obj2 = "escape's this one too";
+
+$array = array("ray"=>"it's escaping arrays", "ray2"=>"escape's this one too");
+
+echo "<pre>".print_r(\Rhonda\Mysql:: real_escape($object),true)."</pre>";
+echo "<pre>".print_r(\Rhonda\Mysql:: real_escape($array),true)."</pre>";
