65823e7daa99c57df2a8328872373837b8f1e719
Matthew Ess <mess@purdue.edu>
2016-07-18 15:37:11 -0400

    Revert "Add JSON test"
    This reverts commit 3d864acf16f6b6e568a7cec9fed6874211b67445.

--- tests/JSON.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-echo "<h3>\Rhonda\JSON</h3>";
-
-// Encode json value with numerics
-$value = array('a' => '1', 'b' => '2', 'c' => '3', 'd' => '4', 'e' => '5', 'string' => 'don\'t convert me!');
-echo \Rhonda\JSON:: encode_numeric($value);
-echo "</br>";
