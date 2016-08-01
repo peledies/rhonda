28f9b38f55cca15825d6a98c30bed497e8abf694
Deac <deac@sdicg.com>
2016-08-01 15:57:37 -0400

    Set response codes with Headers class

--- src/Rhonda/Response.php
+++ src/Rhonda/Response.php
@@ -8,13 +8,13 @@ namespace Rhonda;
  * @uses      \Rhonda\Error
  * @since     2016-07-29
  * @author    Deac Karns <deac@sdicg.com>
- * @author 		Wesley Dekkers
+ * @author    Wesley Dekkers
  */
 
 class Response {
 
   /**
-  * Description Of Module
+  * Package incoming data along with the error summary from \Ronda\Error::summary()
   *
   * In order an error to be populated it uses \Rhonda\Error::add_summary_item(); and \Rhonda\Error::summary();
   *
@@ -35,20 +35,23 @@ class Response {
   * @author  Deac Karns <deac@sdicg.com> 
   * @author  Wesley Dekkers <wesley@sdicg.com> 
   **/
-	public static function package($data){
-		$result = new \stdClass();
+  public static function package($data, $response_code=false){
+    $result = new \stdClass();
     $result->errors = \Rhonda\Error::summary();
     $result->data = $data;
 
     // When error not empty and data not empty change headers 206
-    if(!empty($result->errors) && !empty($data)){
-    	header("HTTP/1.1 209 Partial Success");
+    if(!$response_code && !empty($result->errors) && !empty($data)){
+      $response_code = 209;
     }
     // when error not empty and data empty return 400
-    if(!empty($result->errors) && empty($data)){
-    	header("HTTP/1.1 400 Bad Request");
+    if(!$response_code && !empty($result->errors) && empty($data)){
+      $response_code = 400;
     }
+
+    \Rhonda\Headers:: set_response_code($response_code);
 
     return $result;
   }
+
 }
